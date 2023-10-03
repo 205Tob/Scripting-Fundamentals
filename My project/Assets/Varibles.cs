583f642e3dfaf04ec7eb852663b0d64f924ec76d
Smart-Bizzle <dustinfergusonjr@gmail.com>
2023-10-03 14:22:00 -0700

    Tbizzle is awsome

---
+++ My project/Assets/Varibles.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varibles : MonoBehaviour
+{
+    //Public Global/Member Varibles
+    public int Score = 0; // declare a whole number
+    public float ItemAmount = 19.95f; //decimal number
+    public string PlayerName = "Child_Predetor4473"; // declare a text
+    public bool IsPlayerActivity = true; //declare a true/false
+
+    // private Global/Member Variable
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
