815278e631ae65676a8ae94ea1bbd5efc27c5b1f
Smart-Bizzle <dustinfergusonjr@gmail.com>
2023-10-04 13:14:25 -0700

    Update varibles

--- My project/Assets/Varibles.cs
+++ My project/Assets/Varibles.cs
@@ -11,16 +11,26 @@ public class Varibles : MonoBehaviour
     public bool IsPlayerActivity = true; //declare a true/false
 
     // private Global/Member Variable
-
+    private int _collectibleAmount = 0;
+    private float _horizontalInput = 0f;
+    private string _username = "MoeLester8917";
+    private bool _isGameOver = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // make sure score is equal to 0
+        Score = 0; //assigning a Global/Member varible
+
+        //declare a local varible to track Start Position
+        Vector3 startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // trying to update a local varible value will not work
+        // create a variable to determine object key movement
+        float verticalInput = Input.GetAxis("Vertical");
     }
 }
